e94348019cdfab7aab9bb53a70331440c9aa67e9
donhuan47 <77119844+donhuan47@users.noreply.github.com>
2021-02-12 12:39:32 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,6 +11,7 @@ item2=types.KeyboardButton('Как дела?')
 markup.add(item1, item2)
 
 
+
 bot=telebot.TeleBot("1692964167:AAEMMwSeQVkGUyXJrKSwT0hpMygLhqKAOBc")
 
 
@@ -31,9 +32,40 @@ def lalala(message):
    # bot.send_message(message.chat.id,message.text)
    if message.chat.type=='private':
        if message.text=='Random Number':
-           bot.send_message(message.chat.id, str(random.randint(1,100)))
+        bot.send_message(message.chat.id, str(random.randint(1,100)))
        elif message.text=='Как дела?':
-           bot.send_message(message.chat.id,  'Отлично, а как у вас?')
+         markup = types.InlineKeyboardMarkup(row_width=2)
+         item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+         item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+         markup.add(item1, item2)
+         bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
        else:
-           bot.send_message(message.chat.id,  'Не знаю, что сказать')
+            bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+	   
+
+		   
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+ 
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+                reply_markup=None)
+ 
+            # show alert
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+ 
+    except Exception as e:
+        print(repr(e))
+ 
+# RUN   
+		   
+		   
 bot.polling(none_stop=True)
